425a90766cc2d1d15dc7400d28c3812eece78814
Leon Funnell <leon_funnell@hotmail.com>
2024-09-27 10:43:34 +0100

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,4 +1,10 @@
 provider "google" {
-  project = var.gcp_project_id
-  region  = var.gcp_region
+  credentials = file("service-account-key.json")
+  project     = var.gcp_project_id
+  region      = var.gcp_region
+}
+
+resource "local_file" "service_account_key" {
+  content  = var.service_account_key
+  filename = "${path.module}/service-account-key.json"
 }
